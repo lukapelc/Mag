152d72bc5aa11141a976381fe910a783002ce054
Luka <lukapelc91@gmail.com>
2018-07-06 21:20:03 +0200

    re

--- baza.php
+++ baza.php
@@ -1,10 +1,10 @@
 <?php
- $dbcnx = @mysql_connect('localhost', 'mysqlroot', 'root_groot_2018');
-if (!$dbcnx ) {
-        echo( '<p> Komunikacija s strežnikom SUPB ni uspela . </p>');
-        exit() ;
-    }
-    if(!@mysql_select_db('test', $dbcnx)){
-        die('<p>Zahtevana podatkovna baza ne obstaja.</p>');
-    } 
+ $dbcnx = @mysqli_connect('localhost', 'mysqlroot', 'root_groot_2018');
+//if (!$dbcnx ) {
+  //      echo( '<p> Komunikacija s strežnikom SUPB ni uspela . </p>');
+    //    exit() ;
+    //}
+    //if(!@mysqli_select_db('test', $dbcnx)){
+      //  die('<p>Zahtevana podatkovna baza ne obstaja.</p>');
+   // } 
 ?>
