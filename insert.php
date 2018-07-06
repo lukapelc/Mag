3604f7a47b9faf939d0e980e49e6a63b295d5a31
Luka <lukapelc91@gmail.com>
2018-07-06 21:42:54 +0200

    test

--- insert.php
+++ insert.php
@@ -5,10 +5,10 @@ require_once('baza.php');
 
 if (isset($_POST['ime1'])){
     $name=$_POST['ime1'];
-    $lastname=$_POST['priimek'];
-	$St=$_POST['stevilka'];
-	$query = "INSERT INTO testMarko (ime, priimek, stevilka)  VALUES ' $name', '$lastname' , '$St'";
-	$R1 = mysql_query($query);
+    $lastname=$_POST['priimek1'];
+	$St=$_POST['stevilka1'];
+	$query = "INSERT INTO testMarko (ime, priimek, stevilka)  VALUES ('$name', '$lastname' , '$St')";
+	$R1 = mysqli_query($query);
 	if (!$R1) {
 	die('Invalid query: ' . mysql_error());
 	}
